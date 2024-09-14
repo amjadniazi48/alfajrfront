9aee0d3290d9fee6efddbe7a54ee905b3b78a4a2
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2024-09-14 18:12:59 +0500

    Chnages the heading of dirctor page

--- pages/about-us/directormessage.js
+++ pages/about-us/directormessage.js
@@ -12,7 +12,7 @@ const AboutUs = ({ data }) => {
       <div className="container p-4 mt-20">
         <div className="block-title-6 text-center">
           <h4 className="h5 border-primary">
-            <span className="bg-primary text-white">Chairman Message</span>
+            <span className="bg-primary text-white">Director Message</span>
           </h4>
         </div>
         <div className="row">
