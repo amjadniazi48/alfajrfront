8b248c1234addcfd138dce6e54444fd3c7ccaef2
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2024-09-14 19:00:29 +0500

    Make director and chairman pages responsive

--- pages/about-us/chairmanmessage.js
+++ pages/about-us/chairmanmessage.js
@@ -2,60 +2,65 @@ import React from "react";
 import { API_URL } from "@/config/index";
 import Image from "next/image";
 import Layout from "@/components/Layout";
-
 import ReactMarkdown from "react-markdown";
+
 const AboutUs = ({ data }) => {
-   console.log("about", data);
-  //return false;
   return (
     <Layout title="About Us">
       <div className="container p-4 mt-20">
-      <div className="block-title-6 text-center" >
+        <div className="block-title-6 text-center">
           <h4 className="h5 border-primary">
             <span className="bg-primary text-white">Chairman Message</span>
           </h4>
         </div>
         <div className="row">
-          <div className="col rounded cmessage"   style={{ backgroundColor: "#F7F7F5", border: "1px solid #C2CBCD", textAlign:"justify" }}>
-            <figure class="figure  w-50 float-start m-2">
-              <Image
-          
-                src={
-                  data.About.data.attributes.Messages[0]
-                    ? data.About.data.attributes.Messages[0].image.data[0].attributes.url
-                    : ""
-                }
-                width={325}
-                height={280}
-                style={{
-                  marginLeft: "7px",
-                  marginBottom: "20px",
-                }}
-                alt="About us Images"
-              />
-            </figure>
-            <ReactMarkdown>
-              {data.About.data.attributes.Messages[0].description}
-            </ReactMarkdown>
+          <div
+            className="col-12 rounded cmessage"
+            style={{
+              backgroundColor: "#F7F7F5",
+              border: "1px solid #C2CBCD",
+              textAlign: "justify",
+            }}
+          >
+            <div className="row">
+              <div className="col-lg-4 col-md-5 col-sm-12 mt-3">
+                <figure className="figure w-100">
+                  <Image
+                    src={
+                      data.About.data.attributes.Messages[0]
+                        ? data.About.data.attributes.Messages[0].image.data[0].attributes.url
+                        : ""
+                    }
+                    width={325}
+                    height={280}
+                    layout="responsive" // Responsive image layout
+                    alt="About us Images"
+                  />
+                </figure>
+              </div>
+              <div className="col-lg-8 col-md-7 col-sm-12">
+                <ReactMarkdown>
+                  {data.About.data.attributes.Messages[0].description}
+                </ReactMarkdown>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </Layout>
   );
 };
+
 export async function getServerSideProps({ params }) {
-  //fetching about us page
   const aboutusres = await fetch(`${API_URL}/api/message?populate[Messages][populate][0]=image&populate[Messages][filters][title][$eq]=Chairman Message&publicationState=live&locale[0]=en`);
   const About = await aboutusres.json();
-  //fetching Publications
-  // console.log("job", About);
   return {
     props: {
       data: {
         About,
       },
-      //  revalidate: 10, // In secondss
     },
   };
 }
+
 export default AboutUs;
--- pages/about-us/directormessage.js
+++ pages/about-us/directormessage.js
@@ -2,11 +2,9 @@ import React from "react";
 import { API_URL } from "@/config/index";
 import Image from "next/image";
 import Layout from "@/components/Layout";
-
 import ReactMarkdown from "react-markdown";
+
 const AboutUs = ({ data }) => {
-  console.log("about", data);
-  //return false;
   return (
     <Layout title="About Us">
       <div className="container p-4 mt-20">
@@ -17,70 +15,69 @@ const AboutUs = ({ data }) => {
         </div>
         <div className="row">
           <div
-            className="col rounded cmessage"
-            style={{ backgroundColor: "#F7F7F5", border: "1px solid #C2CBCD", textAlign:"justify" }}>
-          
-            {data.About  && data.About.data.attributes.Messages[0].image.data!=null ? (
-            
-              <figure class="figure  w-50 float-start mt-3 ml-2 mr-2 mb-2">
-                <Image
-                  src={
-                    data.About.data.attributes.Messages[0].image
-                      ? data.About.data.attributes.Messages[0].image?.data[0]?.attributes.url
-                      : ""
-                  }
-                  width={325}
-                  height={280}
-                  style={{
-                    marginLeft: "7px",
-                    marginBottom: "20px",
-                  }}
-                  alt="About us Images"
-                />
-              </figure>
-           
-                
-              ) : (
-                <figure class="figure  w-50 float-start m-4"
-                style={{
-                  width: "325px",
-                  height: "280px",
-                  backgroundColor: "#e0e0e0",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  textAlign: "center",
-                  marginLeft: "7px",
-                  marginBottom: "20px",
-                  color: "#757575",
-                }}
-              >
-                <p>No Image Available</p>
-              </figure>
-            )}
-            <ReactMarkdown>
-              {data.About.data.attributes.Messages[0].description}
-            </ReactMarkdown>
+            className="col-12 rounded cmessage"
+            style={{
+              backgroundColor: "#F7F7F5",
+              border: "1px solid #C2CBCD",
+              textAlign: "justify",
+            }}
+          >
+            <div className="row">
+              <div className="col-lg-4 col-md-5 col-sm-12 mt-3">
+                {data.About && data.About.data.attributes.Messages[0].image.data ? (
+                  <figure className="figure w-100">
+                    <Image
+                      src={
+                        data.About.data.attributes.Messages[0].image?.data[0]?.attributes.url || ""
+                      }
+                      width={325}
+                      height={280}
+                      layout="responsive" // Responsive image layout
+                      alt="Director Message Image"
+                    />
+                  </figure>
+                ) : (
+                  <figure
+                    className="figure w-100"
+                    style={{
+                      width: "325px",
+                      height: "280px",
+                      backgroundColor: "#e0e0e0",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      display: "flex",
+                      color: "#757575",
+                    }}
+                  >
+                    <p>No Image Available</p>
+                  </figure>
+                )}
+              </div>
+              <div className="col-lg-8 col-md-7 col-sm-12">
+                <ReactMarkdown>
+                  {data.About.data.attributes.Messages[0].description}
+                </ReactMarkdown>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </Layout>
   );
 };
+
 export async function getServerSideProps({ params }) {
-  //fetching about us page
   const aboutusres = await fetch(
     `${API_URL}/api/message?populate[Messages][populate][0]=image&populate[Messages][filters][title][$eq]=Director Message&publicationState=live&locale[0]=en`
   );
   const About = await aboutusres.json();
-  //fetching Publications
-  // console.log("job", About);
   return {
     props: {
       data: {
         About,
       },
-      //  revalidate: 10, // In secondss
     },
   };
 }
+
 export default AboutUs;
