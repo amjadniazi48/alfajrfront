733904ebb6be5b801251ba605cfa6008bb087af2
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2024-09-21 21:23:51 +0500

    Added vital links and icons on vital links and downloads

--- pages/vital-link/index.js
+++ pages/vital-link/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { API_URL } from "@/config/index";
 import Link from "next/link";
-import ReactPaginate from "react-paginate";
+
 import moment from "moment";
 import Image from "next/image";
 import Layout from "@/components/Layout";
@@ -16,42 +16,21 @@ const HighLights = () => {
     setLoading(true);
     const getHighlights = async () => {
       const res = await fetch(
-        `${API_URL}/api/downloads?populate[downloadfile][fields][0]=name&populate[downloadfile][fields][1]=url&pagination[pageSize]=${limit}&pagination[page]=1&publicationState=live&locale[0]=en`
+        `${API_URL}/api/vital-link?populate[Links][fields][0]=href&populate[Links][fields][1]=label&populate[Links][fields][2]=target&pagination[pageSize]=${limit}&pagination[page]=1&status=published&locale[0]=en`
         // `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=${limit}`
       );
       const data = await res.json();
       console.log("hightlights", data);
-
-      const total = data && data.meta.pagination.total;
-
-      setpageCount(Math.ceil(total / limit));
-      // console.log(Math.ceil(total/12));
+     
       setLoading(false);
       setItems(data);
     };
 
     getHighlights();
   }, [limit]);
-  // console.log("items", items);
-  const fetchPublications = async (currentPage) => {
-    const res = await fetch(
-      `${API_URL}/api/downloads?populate[downloadfile][fields][0]=name&populate[downloadfile][fields][1]=url&pagination[pageSize]=${limit}&pagination[page]=${currentPage}&publicationState=live&locale[0]=en`
-      // `https://jsonplaceholder.typicode.com/comments?_page=${currentPage}&_limit=${limit}`
-    );
-    const data = await res.json();
-    return data;
-  };
-  const handlePageClick = async (data) => {
-    console.log(data.selected);
-
-    let currentPage = data.selected + 1;
-
-    const publicationFromServer = await fetchPublications(currentPage);
-
-    setItems(publicationFromServer);
-  };
+ 
   return (
-    <Layout title="Highlights">
+    <Layout title="Vital Links">
       <div className="wrapper ">
         {/* main content */}
         <main id="content">
@@ -60,14 +39,14 @@ const HighLights = () => {
               <div className="col-sm-12">
                 <div className="block-title-6 text-center">
                   <h4 className="h5 border-primary">
-                    <span className="bg-primary text-white">Downloads</span>
+                    <span className="bg-primary text-white">Vital Links</span>
                   </h4>
                 </div>
                 <table className="table table-striped table-bordered">
                   <thead className="table-light">
                     <tr>
                       <td>Title</td>
-                      <td>Download</td>
+                      <td>Link</td>
                       <td>Dated</td>
                     </tr>
                   </thead>
@@ -88,29 +67,29 @@ const HighLights = () => {
                       </tr>
                     ) : (
                       items &&
-                      items.data?.map((highlight) => {
+                      items.data?.attributes.Links.map((highlight) => {
                         return (
-                          <tr key={highlight.attributes.id}>
-                            <td>{highlight.attributes.title}</td>
+                          <tr key={highlight.id}>
+                            <td>{highlight.label}</td>
                             <td>
                               {" "}
                               <Link
                                 href={
-                                  highlight.attributes.downloadfile.data
-                                    .attributes.url
+                                  highlight.href
                                 }
-                                target="_blank"
+                                target={highlight.target}
                               >
                                 <button className="btn btn-danger p-2">
-                                  Download File &nbsp; <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-arrow-down-fill" viewBox="0 0 16 16">
-  <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1m-1 4v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 11.293V7.5a.5.5 0 0 1 1 0"/>
+                                 Link &nbsp;&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
+  <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
 </svg>
                                 </button>
                               </Link>
                             </td>
                             <td>
                               <time dateTime="2019-10-21">
-                                {moment(highlight.attributes.createdAt).format(
+                                {moment(highlight.createdAt).format(
                                   "Do MMMM YYYY"
                                 )}
                               </time>
@@ -121,25 +100,7 @@ const HighLights = () => {
                     )}
                   </tbody>
                 </table>
-                <ReactPaginate
-                  previousLabel={"previous"}
-                  nextLabel={"next"}
-                  breakLabel={"..."}
-                  pageCount={pageCount}
-                  marginPagesDisplayed={2}
-                  pageRangeDisplayed={3}
-                  onPageChange={handlePageClick}
-                  containerClassName={"pagination justify-content-center"}
-                  pageClassName={"page-item"}
-                  pageLinkClassName={"page-link"}
-                  previousClassName={"page-item"}
-                  previousLinkClassName={"page-link"}
-                  nextClassName={"page-item"}
-                  nextLinkClassName={"page-link"}
-                  breakClassName={"page-item"}
-                  breakLinkClassName={"page-link"}
-                  activeClassName={"active"}
-                />
+              
               </div>
             </div>
           </div>
